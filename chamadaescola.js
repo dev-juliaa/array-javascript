593d8190647c8152086f7849213e9665f6ed4d34
julia <63320916+dev-juliaa@users.noreply.github.com>
2023-04-23 17:43:04 -0300

    Add files via upload

---
+++ chamadaescola.js
@@ -0,0 +1,8 @@
+// o progessor precisa realizar a chamada, porem ana e caio sairam da escola e rodrigo entrou
+// utilizamos o elemento splice para remover e substituir os alunos
+
+const alunos = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+alunos.splice(1, 2, "Rodrigo"); 
+
+console.log(alunos)
