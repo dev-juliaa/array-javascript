ab326b7ced545330fea905d05e27a90d54001b23
julia <63320916+dev-juliaa@users.noreply.github.com>
2023-04-23 16:55:43 -0300

    Update corrigindomedia.js

--- corrigindomedia.js
+++ corrigindomedia.js
@@ -1,3 +1,7 @@
+// o professor ao adicionar as notas, esqueceu da ultima nota, logo não conseguiria realizar a media
+// adicionamos o comando push para ajuda-lo
+
+
 const nota = [10, 6, 8];
 
 nota.push(7);
