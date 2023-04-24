a8ffaab8c67007d2795010fb45a77c8b2f6cee33
julia <63320916+dev-juliaa@users.noreply.github.com>
2023-04-24 10:38:21 -0300

    Add files via upload

---
+++ juntandosala.js
@@ -0,0 +1,11 @@
+//precisamos juntar as salas de js com a de react
+//com isso utilizamos o elemnto concat para juntar os dois sem alterar a ordem.
+
+
+const salaJS = ["Maria", "Luana", "Eduarda"];
+const salaReact = ["Pedro", "João", "Marcelo"];
+
+const salasjuntas = salaJS.concat(salaReact);
+
+console.log(salasjuntas);
+
