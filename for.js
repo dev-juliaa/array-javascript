fbcd0339ce9c006597dfd0079f259cdc695c5c9e
julia <63320916+dev-juliaa@users.noreply.github.com>
2023-04-25 17:52:07 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,10 @@
+const numero = [100, 200, 300, 400, 500, 600];
+
+//primeira expressao: é executada apenas uma unica vez
+//segunda expressao: ccondição de execução
+// terceira expressao: é executada sempre ao final do bloco
+
+
+for(let indice = 0; indice < numero.length; indice++){
+   console.log(numero[indice]);
+}
---
+++ lista-duas-dimenções.js
@@ -0,0 +1,7 @@
+const alunos = ["Joao", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`a aluna da posição 3 da lista de chamada é: ${listaDeAlunosEMedias[0][2]} 
+e sua nota é: ${listaDeAlunosEMedias[1][2]}`);
